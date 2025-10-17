29e3079bac2b5f303067f43c3dcd1911de68176b
kuzik <bogdankuzik2509@gmail.com>
2025-10-17 16:58:06 +0200

    add pre-commit with black

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Category, Product, Customer, Order, OrderItem
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price", "stock", "available")
+    list_filter = ("category", "available")
+    search_fields = ("name",)
+    list_editable = ("price", "stock", "available")
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "phone")
+    search_fields = ("first_name", "last_name", "email")
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer", "created_at", "status")
+    list_filter = ("status", "created_at")
+    search_fields = ("customer__first_name", "customer__last_name", "customer__email")
+    inlines = [OrderItemInline]
