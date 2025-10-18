ed0a08be83269bc3f60d22d68b807b0f89166abf
kuzik <bogdankuzik2509@gmail.com>
2025-10-18 11:36:16 +0200

    add two templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from catalog.models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "catalog/product_list.html"
+    context_object_name = "products"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "catalog/product_detail.html"
+    context_object_name = "product"
