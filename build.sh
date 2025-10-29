a54361a9fa5bf9e3bc01ce2c16aa27a54dd0236e
kuzik <bogdankuzik2509@gmail.com>
2025-10-29 15:56:30 +0100

    add auto admin

--- build.sh
+++ build.sh
@@ -11,3 +11,6 @@ python manage.py collectstatic --no-input
 # Run migrations
 python manage.py migrate
 
+# Create superuser if it doesn't exist (only creates if not exists)
+python manage.py create_admin || true
+
